4ab43e812e72b3b8bdfcb89f906869b1251b33ea
Laura Kambanis <kambanislaura@gmail.com>
2023-09-27 08:43:27 +0100

    added hide next or prev button functionality

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ slides.forEach(function (slide, index) {
   slide.style.left = `${index * 100}%`;
 });
 
+//keeps track of the current slide being displayed
 let counter = 0;
 
 nextBtn.addEventListener("click", function () {
@@ -24,7 +25,39 @@ prevBtn.addEventListener("click", function () {
 // slide-container by -0% or -100%, - 200% etc to the left
 
 function carousel() {
+  //working with slides
+  //counter changes dependent on next/prev button event listener
+  //slides variable holds all slides with class slide.
+
+  //if counter exceeds the number of slides then go back to slide 1 (index 0)
+  // if (counter === slides.length) {
+  //   counter = 0;
+  // }
+
+  // //if the counter is negative back from one, goes back to last slide
+  // if (counter < 0) {
+  //   counter = slides.length - 1;
+  // }
+
+  // button visibility - whether to display next or previous based on counter value
+
+  //can click next until last slide.
+
+  if (counter < slides.length - 1) {
+    nextBtn.style.display = "block";
+  } else {
+    nextBtn.style.display = "none";
+  }
+
+  //can click previous if not first slide
+  if (counter > 0) {
+    prevBtn.style.display = "block";
+  } else prevBtn.style.display = "none";
+
+  //translate (move each slide) based on counter variable (slide);
   slides.forEach(function (slide) {
     slide.style.transform = `translateX(-${counter * 100}%)`;
   });
 }
+
+prevBtn.style.display = "none";
